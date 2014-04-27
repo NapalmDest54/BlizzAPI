8987eaf8b8f09d03e4255130964478290ea76b70
MercenaryIII <cfj90210@gmail.com>
2014-04-27 13:06:20 -0400

    Added Json properties for the Boss class.

--- BlizzAPI/WoW/character/Boss.cs
+++ BlizzAPI/WoW/character/Boss.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,15 +9,34 @@ namespace BlizzAPI.WoW.character
 {
     public class Boss
     {
-        public int id { get; set; }
-        public string name { get; set; }
-        public int normalKills { get; set; }
-        public object normalTimestamp { get; set; }
-        public int? heroicKills { get; set; }
-        public long? heroicTimestamp { get; set; }
-        public int? lfrKills { get; set; }
-        public long? lfrTimestamp { get; set; }
-        public int? flexKills { get; set; }
-        public long? flexTimestamp { get; set; }
+        [JsonProperty("id")]
+        public int ID { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("normalKills")]
+        public int NormalKills { get; set; }
+
+        [JsonProperty("normalTimestamp")]
+        public object NormalTimestamp { get; set; }
+
+        [JsonProperty("heroicKills")]
+        public int HeroicKills { get; set; }
+
+        [JsonProperty("heroicTimestamp")]
+        public long HeroicTimestamp { get; set; }
+
+        [JsonProperty("lfrKills")]
+        public int LFRKills { get; set; }
+
+        [JsonProperty("lfrTimestamp")]
+        public long LFRTimestamp { get; set; }
+
+        [JsonProperty("flexKills")]
+        public int FlexKills { get; set; }
+
+        [JsonProperty("flexTimestamp")]
+        public long FlexTimestamp { get; set; }
     }
 }
