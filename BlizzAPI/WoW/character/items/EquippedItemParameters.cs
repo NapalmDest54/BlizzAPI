270777a0544a58ea7dc8cd407c9d7d015547fc99
NapalmDest54 <napalmdest54@gmail.com>
2014-05-02 17:18:11 -0400

    Added reforge from and to details.
    #5

--- BlizzAPI/WoW/character/items/EquippedItemParameters.cs
+++ BlizzAPI/WoW/character/items/EquippedItemParameters.cs
@@ -1,13 +1,62 @@
-﻿using Newtonsoft.Json;
+﻿// Copyright (C) 2011 by Sherif Elmetainy (Grendiser@Kazzak-EU)
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+
 namespace BlizzAPI.WoW.character.items
 {
     public class EquippedItemParameters
     {
+        private static readonly Dictionary<int, BlizzAPI.WoW.Items.ItemStats.ItemStatType[]> reforgeIds = InitializeReforgeIds();
+
+        private static Dictionary<int, BlizzAPI.WoW.Items.ItemStats.ItemStatType[]> InitializeReforgeIds()
+        {
+            var reforgeIds = new Dictionary<int, BlizzAPI.WoW.Items.ItemStats.ItemStatType[]>();
+            var statTypes = new BlizzAPI.WoW.Items.ItemStats.ItemStatType[] { 
+                BlizzAPI.WoW.Items.ItemStats.ItemStatType.Spirit, 
+                BlizzAPI.WoW.Items.ItemStats.ItemStatType.DodgeRating, 
+                BlizzAPI.WoW.Items.ItemStats.ItemStatType.ParryRating,
+                BlizzAPI.WoW.Items.ItemStats.ItemStatType.HitRating,
+                BlizzAPI.WoW.Items.ItemStats.ItemStatType.CritRating,
+                BlizzAPI.WoW.Items.ItemStats.ItemStatType.HasteRating,
+                BlizzAPI.WoW.Items.ItemStats.ItemStatType.ExpertiseRating,
+                BlizzAPI.WoW.Items.ItemStats.ItemStatType.MasteryRating
+            };
+            var startReforgeId = 113;
+            for (int i = 0; i < statTypes.Length; i++)
+            {
+                for (int j = 0; j < statTypes.Length; j++)
+                {
+                    if (i != j)
+                    {
+                        reforgeIds.Add(startReforgeId++, new BlizzAPI.WoW.Items.ItemStats.ItemStatType[] { statTypes[i], statTypes[j] });
+                    }
+                }
+            }
+            return reforgeIds;
+        }
 
         [JsonProperty("gem0")]
         public int? Gem0 { get; set; }
@@ -28,10 +77,7 @@ namespace BlizzAPI.WoW.character.items
         public List<int> Set { get; set; }
 
         [JsonProperty("reforge")]
-        /// <summary>
-        /// Gets or sets the reforge ID.
-        /// </summary>
-        public int Reforge { get; set; }
+        public int? Reforge { get; set; }
 
         [JsonProperty("upgrade")]
         public EquippedItemUpgrade Upgrade { get; set; }
@@ -45,5 +91,30 @@ namespace BlizzAPI.WoW.character.items
         [JsonProperty("tinker")]
         public int? Tinker { get; set; }
 
+        /// <summary>
+        /// Gets the reforged from stat
+        /// </summary>
+        public BlizzAPI.WoW.Items.ItemStats.ItemStatType? ReforgedFromStat
+        {
+            get
+            {
+                if (!Reforge.HasValue)
+                    return null;
+                return reforgeIds[Reforge.Value][0];
+            }
+        }
+
+        /// <summary>
+        /// Gets the reforged to stat
+        /// </summary>
+        public BlizzAPI.WoW.Items.ItemStats.ItemStatType? ReforgedToStat
+        {
+            get
+            {
+                if (!Reforge.HasValue)
+                    return null;
+                return reforgeIds[Reforge.Value][1];
+            }
+        }
     }
 }
